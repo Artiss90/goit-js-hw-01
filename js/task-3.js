373bb97611a07e1281f9d8f8514b152282a001fd
Art <Petrenkoartik@gmail.com>
2020-08-13 23:12:27 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,30 @@
 'use strict';
+const userPassword = '';
+
+const ADMIN_PASSWORD = 'jqueryismyjam';
+
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+let message;
+
+if (userPassword === ADMIN_PASSWORD) {
+  message = ACCESS_IS_ALLOWED;
+} else if (userPassword === '') {
+  message = CANCELED_BY_USER;
+} else if (userPassword != ADMIN_PASSWORD) {
+  message = ACCESS_DENIED;
+}
+console.log(message);
+
+//если userPassword равно 'jqueryismyjam'
+// то значение message будет равно
+// 'Добро пожаловать!'
+
+//если userPassword равно  null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если userPassword равно '123'
+// то значение message будет равно
+// 'Доступ запрещен, неверный пароль!'
